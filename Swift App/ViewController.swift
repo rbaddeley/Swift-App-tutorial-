99c0b14bc2dcc28bddab5eb076e6aeb235cfd060
Robert Baddeley <robertbaddeley@roberts-imac.local>
2016-11-22 21:22:07 -1000

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Robert Baddeley on 11/22/16.
+//  Copyright © 2016 Robert Baddeley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
